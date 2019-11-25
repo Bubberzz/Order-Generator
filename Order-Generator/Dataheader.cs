a7be332164f3dd612002776b0a951075cad2d1d3
Stan Bubbers <stan.bubbers@live.co.uk>
2019-11-25 01:08:29 +0000

    datagrid for dataheader working

---
+++ Order-Generator/Dataheader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Order_Generator
+{
+    public partial class Dataheader : MainWindow
+    {
+        public List<Data> Employees { get; set; }
+        public List<string> Genders { get; set; }
+
+
+        //Range getExcel = GetExcelData.getExcelData();
+
+        //public Dataheader()
+        //{
+        //    Employees = new List<Data>()
+        //    {
+        //        new Data() { ID = getExcel.ID, Address1 = getExcel.Address },
+                
+        //    };
+
+            
+        //    InitializeComponent();
+        //    TBC.ItemsSource = Employees;
+           
+        //}
+
+        //private void ShowPersonDetails_Click(object sender, RoutedEventArgs e)
+        //{
+        //    foreach (Data employee in Employees)
+        //    {
+        //        string text = string.Empty;
+        //        text = "Name : " + employee.Name + Environment.NewLine;
+        //        text += "Gender : " + employee.Gender + Environment.NewLine;
+        //        MessageBox.Show(text);
+        //    }
+        //}
+    }
+}
--- Order-Generator/MainWindow.xaml.cs
+++ Order-Generator/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Office.Interop.Excel;
+using Type = System.Type;
+using Window = System.Windows.Window;
+using System.Data;
+using DataTable = System.Data.DataTable;
 
 namespace Order_Generator
 {
@@ -20,9 +25,92 @@ namespace Order_Generator
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Data> PackOrder { get; set; }
+        DataTable getExcel = GetExcelData.getExcelData();
+        //DataTable dt = new DataTable();
+
         public MainWindow()
         {
             InitializeComponent();
+            TBC.ItemsSource = getExcel.DefaultView;
+
+
+        }
+
+        private void Card_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch
+            {
+                //ignored
+            }
+        }
+
+        private void dataheaderBtnClicked(object sender, RoutedEventArgs e)
+        {
+            mainDrawer.IsLeftDrawerOpen = false;
+            dataheaderRadio.IsChecked = true;
+            datalinesRadio.IsChecked = false;
+        }
+
+        private void datalinesBtnClicked(object sender, RoutedEventArgs e)
+        {
+            mainDrawer.IsLeftDrawerOpen = false;
+            dataheaderRadio.IsChecked = false;
+            datalinesRadio.IsChecked = true;
+        }
+
+        private void closeBtnClicked(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void dataheaderTextBoxClick(object sender, MouseButtonEventArgs e)
+        {
+            dataheaderTextBox.Text = "";
+        }
+
+        private void dataheaderTextBoxLostFocus(object sender, RoutedEventArgs e)
+        {
+            dataheaderTextBox.Text = "Enter Dataheader ID";
+        }
+
+        private void loadBtnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception exception)
+            {
+                
+            }
+
+            //var dataheaderID = Convert.ToInt32(dataheaderTextBox.Text);
+
+            int dataheaderID = 3;
+
+            //int.TryParse(dataheaderTextBox.Text, out dataheaderID);
+            //dt.Rows.Add();
+
+            //getExcel.Rows.Add(textBoxID.Text, textBoxFN.Text, textBoxLN.Text, textBoxAGE.Text);
+
+            //var query = from o in getExcel
+            //    select
+
+            TBC.ItemsSource = (System.Collections.IEnumerable)getExcel;
+
+            //Cast<GetExcelData>() = getExcel
+            //.Where(item => item.ID == "3")
+            //.Select(item => item.Make);
+
+
+            //getExcel
+            //.AsEnumerable()
+            //.Where(myRow => myRow.Field<string>("ID") == "3");
         }
     }
 }
